07d745a742e609ea5c904149d46f49de84f82fe1
Shubham-Dabhade <91594783+Shubham-Dabhade@users.noreply.github.com>
2023-11-06 19:47:54 +0530

    Add files via upload

---
+++ public/index.js
@@ -0,0 +1,32 @@
+
+
+// var headerscroll = $("#header").height() + $("#section1").height() ;
+
+// var headerScene = new ScrollMagic.Scene({
+//   pushFollowers: false,
+//   duration: headerscroll,
+//   offset: 30
+// })
+// .setPin("#header");
+
+// controller.addScene([
+//   headerScene
+// ]);
+
+// function splitScroll(){
+
+//     const controller = new ScrollMagic.Controller();
+
+//     new ScrollMagic.Scene({
+//         pushFollowers: false,
+//         duration: 500,
+//         triggerHook:0,
+//         triggerElement:".solution-track-right-container"
+//     })
+//     .setPin(".solution-track-right-container")
+//     .addIndicators()
+//     .addTo(controller);
+
+
+// }
+// splitScroll();
